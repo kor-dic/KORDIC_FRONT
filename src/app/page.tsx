e5b99dc4c031fca09b41eaa299511dc93e3a6738
유림 <yulimh001@gmail.com>
2025-09-13 13:24:04 +0900

    feat: next.js 초기 세팅

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center">
+      <div className="w-[400px] bg-[white] min-h-screen">
+        <button className="bg-conic-orange-30 text-white button-lg py-3 w-full rounded-full">시작하기</button>
+      </div>
+    </main>
+  );
+}
